925e1b38390624d0eb1b209f7d4f45468402c734
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-08-03 00:50:08 +0300

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { list } from 'postcss';
 import getListings, { IListingsParams } from './actions/getListings';
 import ClientOnly from './components/ClientOnly';
